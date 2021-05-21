f4575f3b3884fe55d3746162e088dfa43525e0fb
ulascandundar <79878694+ulascandundar@users.noreply.github.com>
2021-05-22 01:27:07 +0300

    first

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Employee.java
@@ -0,0 +1,49 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue
+	@Column(name ="user_id")
+	private int user_id;
+	
+	@Column(name ="first_name")
+	private String first_name;
+	
+	@Column(name ="last_name")
+	private String last_name;
+	
+	public Employee(int user_id, String first_name, String last_name) {
+		super();
+		this.user_id = user_id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	
+}
